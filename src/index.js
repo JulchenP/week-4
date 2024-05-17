ead6c2b4eb86b990fad24be2e751beaad98ffcb2
Julia P <kiwisocke@gmail.com>
2024-05-17 14:21:48 +0200

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Search from './Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Search from "./Search";
-import "./styles.css";
+import App from "./App";
+import "./App.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -15,7 +14,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode class="Search">
     <h1>Weather App</h1>
-    <Search />
+    <App />
   </StrictMode>
 );
 
