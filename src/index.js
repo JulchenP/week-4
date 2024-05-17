317dbed57f0aa823739403e14c30746171c1c16c
Julia P <kiwisocke@gmail.com>
2024-05-17 14:27:57 +0200

    trial debugging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -12,11 +7,8 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode class="Search">
+  <StrictMode class="App">
     <h1>Weather App</h1>
     <App />
   </StrictMode>
 );
-
-
-
